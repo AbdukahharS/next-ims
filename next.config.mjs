ee9c9b8b75dcb002deb4cdf0c9b39f8e237b29cd
AbdukahharS <shakhzodbekkakhkhorov04@gmail.com>
2024-08-25 17:09:39 +0800

    config commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,16 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+import withPWA from 'next-pwa'
 
-export default nextConfig;
+const withPWAConfig = withPWA({
+  dest: 'public',
+  disable: process.env.NODE_ENV === 'development',
+  register: true,
+  skipWaiting: true,
+})
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+}
+
+export default withPWAConfig(nextConfig)
