3cb78e556e62df40af15038bf1c22a168a34b861
AbdukahharS <shakhzodbekkakhkhorov04@gmail.com>
2024-10-07 23:38:45 +0800

    half page print v2

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,15 @@
 import withPWA from 'next-pwa'
 
-const withPWAConfig = {
+const withPWAConfig = withPWA({
   dest: 'public',
   disable: process.env.NODE_ENV === 'development',
   register: true,
   skipWaiting: true,
+})
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
 }
 
-export default withPWA(withPWAConfig)
+export default withPWAConfig(nextConfig)
